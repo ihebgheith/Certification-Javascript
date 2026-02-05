b38019aca507f3eeed48374c094a15d9375bf613
ihebgheith <31036208+ihebgheith@users.noreply.github.com>
2026-02-05 16:33:44 +0100

    05022026

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const instrumentsArr =[
+{category : "woodwinds",
+ instrument : "Flute",
+   price : 500 
+
+},
+{category : "woodwinds",
+ instrument : "clarinet",
+   price : 200 
+
+},
+{category : "woodwinds",
+ instrument : "oboe",
+   price : 4000 
+
+}
+
+]
